df314ee548fbbd4ebaf723544ce27516592f84d7
Ashok Modhave <ashok.modhave143@gmail.com>
2018-09-29 21:16:51 +1200

    Initial application base set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+// Custom Components Import
+import { Register } from "./components/Register/Register";
+import { SideDrawer } from "./components/SideDrawer/SideDrawer";
+import { Toolbar } from "./components/Toolbar/Toolbar";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        Main Container
+        <Toolbar props={{}} />
+        <SideDrawer props={{}} />
+        <Register props={{}} />
       </div>
     );
   }
