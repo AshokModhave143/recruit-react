2f50effae3df84b5b3a8f54d5724248f6b9aaaa3
Ashok Modhave <ashok.modhave143@gmail.com>
2018-09-30 11:06:00 +1300

    Adding all components into main container and run it

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import * as React from 'react';
 import './App.css';
 
 // Custom Components Import
+import { Backdrop } from "./components/Backdrop/Backdrop";
+import { Menubar } from "./components/Menubar/Menubar";
 import { Register } from "./components/Register/Register";
 import { SideDrawer } from "./components/SideDrawer/SideDrawer";
-import { Toolbar } from "./components/Toolbar/Toolbar";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        Main Container
-        <Toolbar props={{}} />
+        <Menubar props={{}} />
         <SideDrawer props={{}} />
-        <Register props={{}} />
+        <Backdrop props={{}} />
+        <main className="App-main">
+          <Register props={{}} />
+        </main>
       </div>
     );
   }
